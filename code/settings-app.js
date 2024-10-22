1cc3e7ad06a87a3844c14ca19cc0adf8cd1cb60c
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-10-22 23:32:14 +0500

    update

--- code/settings-app.js
+++ code/settings-app.js
@@ -18,17 +18,42 @@ const fields = {
     percent: '#percent'
 };
 
-const maskOptions = {
-    mask: '\d+ %'
-};
-const mask = IMask(percent_input, maskOptions);
+function mask(id){
+    const elm = document.getElementById(id);
+    const suffix = ' %';
+    const bypass = [9, 16, 17, 18, 36, 37, 38, 39, 40, 91, 92, 93];
+    
+    const saveValue = (data) => {
+      elm.dataset.value = data;
+    };
+    
+    const pureValue = () => {
+      let value = elm.value.replace(/\D/g, '');
+      value = parseInt(value.replace(suffix, ''))
+      return value || '';
+    };
+    
+    const focusNumber = () => {
+      elm.setSelectionRange(elm.dataset.value.length, elm.dataset.value.length);
+    };
+    
+    elm.addEventListener('keyup', (e) => {
+      if (bypass.indexOf(e.keyCode) !== -1) return;
+      
+      const pure = pureValue();
+      saveValue(pure);
+      
+      if (!pure) {
+        elm.value = '';
+        return;
+      }
+  
+      elm.value = pure + suffix;
+      focusNumber();
+    });
+  }
 
-// $(document).ready(function(){
-//     $('#partner-percent').inputmask({
-//         alias: 'numeric',
-//         suffix: ' %'
-//     });
-// });
+  mask(percent_input);
 
 function show(check) {
     let d = check ? 'flex' : 'none';
