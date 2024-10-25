5dcc997570fc6f93e8f14f6ae46c07b34bda10e3
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-10-25 21:43:30 +0500

    update

--- code/pre-calc.js
+++ code/pre-calc.js
@@ -113,14 +113,14 @@ tg.onEvent('mainButtonClicked', async (event) => {
             const response = await fetch(`/do-calculation?partner=${partner}&name=${encodeURIComponent(name)}&phone=${encodeURIComponent(phone)}&brand=${encodeURIComponent(brand)}&model=${encodeURIComponent(model)}&gosnum=${encodeURIComponent(gosnum)}`);
             const { link } = await response.json();
             if (link) {
-                tg.showPopup({message: 'Расчет успешно сформирован'});
+                tg.showPopup({ title: 'Success', message: 'Расчет успешно сформирован' });
                 tg.MainButton.hideProgress();
 
                 event.openLink(link);
                 // tg.mainButton.setParams({
                 //     has_shine_effect: true,
                 //     text: 'Открыть расчет',
-                   
+
                 // });
             }
         } catch (error) {
