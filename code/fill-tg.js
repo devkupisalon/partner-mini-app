5dbd0507ebb83a5b7af66aa2dcbf279185f4a62e
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-10-31 02:35:51 +0500

    update

--- code/fill-tg.js
+++ code/fill-tg.js
@@ -1,16 +1,27 @@
+const fetchCheck = async (string) => {
+    try {
+        const response = await fetch(`/validate-init?${string}`);
+        const data = await response.json();
+        console.log(data);
+        return data ? true : false;
+    } catch (error) {
+        console.error('Error:', error);
+    }
+};
+
 fill_tg.addEventListener('click', async () => {
     await tg.requestContact(async (shared, callback) => {
-      if (shared && callback) {
-        const check = await fetchCheck(callback.response);
-        console.log(callback);
-        if (check) {
-          const contact = callback.responseUnsafe.contact;
-          n.value = `${contact.first_name} ${contact.last_name}`;
-          ph.value = contact.phone_number;
-          setCheckmark(fill_tg);
-        } else {
-          console.error('Data is not valid');
+        if (shared && callback) {
+            const check = await fetchCheck(callback.response);
+            console.log(callback);
+            if (check) {
+                const contact = callback.responseUnsafe.contact;
+                n.value = `${contact.first_name} ${contact.last_name}`;
+                ph.value = contact.phone_number;
+                setCheckmark(fill_tg);
+            } else {
+                console.error('Data is not valid');
+            }
         }
-      }
     });
-  });
+});
