086b9591c5f75646ca3a76d08b6a451c3e17456f
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-10-30 20:52:58 +0500

    update

--- code/mini-app.js
+++ code/mini-app.js
@@ -26,7 +26,7 @@ tg.enableClosingConfirmation();
 let { user: { username, id }, start_param } = tg.initDataUnsafe;
 start_param = start_param !== undefined ? start_param : partner;
 console.log(`partner: ${partner}`);
-console.log(`start_param: ${start_param}`);
+console.log(`start_param: ${typeof start_param}`);
 
 const setCheckmark = s => {
     s.style.pointerEvents = "none";
