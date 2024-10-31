c5e58fe8df5a75e0b4cd479a213426c51277c34c
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-11-01 00:09:32 +0500

    update

--- functions/sheets.js
+++ functions/sheets.js
@@ -289,7 +289,7 @@ const save_new_partner = async (params) => {
 const save_logo = async (params) => {
     const { body: { name, folder }, file } = params;
     // const filePath = path.join(__dirname, 'logos');
-    logger.info(JSON.parse(file).buffer.data);
+    logger.info(typeof file);
     const fileBuffer = Buffer.from(Array.prototype.slice.call(file.buffer.data));
     const fileDataInBase64 = fileBuffer.toString('base64');
     logger.info(fileDataInBase64);
