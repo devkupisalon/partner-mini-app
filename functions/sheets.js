a05e1d70cac063aefd294df5e2157c6c05589085
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-11-01 22:30:05 +0500

    update

--- functions/sheets.js
+++ functions/sheets.js
@@ -177,6 +177,7 @@ const save_settings = async (obj) => {
  */
 const get_settings = async (partner) => {
     try {
+        logger.info(partner);
         if (partner !== undefined || partner !== null) {
             const values = await get_data(DB, DATASHEETNAME);
             const column_index = getColumnNumberByValue(values[0], VALUE) - 1;
