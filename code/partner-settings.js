56a6b604be4ba4fc30792b767f83965bca9c0f96
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-11-01 17:39:52 +0500

    update

--- code/partner-settings.js
+++ code/partner-settings.js
@@ -12,8 +12,10 @@ async function show_percent() {
 
     if (selected_work_type === partner_type) {
         show(true);
+        logo?.style.display = "block";
     } else {
         show(false);
+        logo?.style.display = "none";
     }
 
     work_type_input.onchange = show_percent;
