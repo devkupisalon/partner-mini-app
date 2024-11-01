fb8fe4e679c0115e5faed3103b4064e6215ea52c
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-11-01 10:54:55 +0500

    update

--- code/mini-app.js
+++ code/mini-app.js
@@ -24,10 +24,8 @@ let work_type_partner, percent_partner, root;
 tg.enableClosingConfirmation();
 
 let { user: { username, id }, start_param } = tg.initDataUnsafe;
+console.log(start_param);
 start_param = start_param !== undefined ? start_param : partner;
-
-console.log(`partner: ${partner}`);
-console.log(`start_param: ${start_param}`);
 
 /**
  * Установить галочку для элемента и отключить его
