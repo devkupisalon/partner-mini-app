4cfebd8c198c1849fabc713b6330023a892c0d37
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-11-03 03:28:53 +0500

    update

--- functions/process-check-moderation.js
+++ functions/process-check-moderation.js
@@ -10,28 +10,36 @@ const { DB, DATASHEETNAME } = constants;
 const task = cron.schedule('* * * * *', async () => {
     try {
         const data_obj = await check_success_moderation();
+        const success_obj = {};
 
         if (Object.keys(data_obj).length > 0) {
-            await Promise.all(Object.values(data_obj).map(async ({ chat_id, type, uid, i, col_letter }) => {
+            Object.values(data_obj).forEach(async ({ chat_id, type, uid, i, col_letter }, index) => {
                 try {
                     const success = await send_first_messages(chat_id, type, uid);
                     if (success) {
-                        const range = `${DATASHEETNAME}!${col_letter}${i}`;
-                        const requestBody = { values: [[true]] };
-                        const { data } = await update_data(DB, range, requestBody);
-                        await new Promise(resolve => setTimeout(resolve, 4000));
-                        logger.info(data);
+                        if (!success_obj[index]) success_obj[index] = {};
+                        success_obj[index] = { col_letter, i };
                         logger.info('Initial messages sent successfully');
-                        if (data.spreadsheetId) {
-                            logger.info(`Check_server set to TRUE`);
-                        }
                     }
                 } catch (error) {
                     logger.error(`Error sending initial messages: ${error}`);
                 }
-            }));
+                await new Promise(resolve => setTimeout(resolve, 1000));
+            });
         } else {
             logger.info('There are no users to send initial messages');
+        }
+
+        if (Object.keys(success_obj).length > 0) {
+            Object.values(success_obj).forEach(async ({ col_letter, i }) => {
+                const range = `${DATASHEETNAME}!${col_letter}${i}`;
+                const requestBody = { values: [[true]] };
+                const { data } = await update_data(DB, range, requestBody);
+                logger.info(data);
+                if (data.spreadsheetId) {
+                    logger.info(`Check_server set to TRUE`);
+                }
+            });
         }
     } catch (error) {
         logger.error(`An error occurred in cron schedule: ${error}`);
