fb207bc25868d84c6250b5f5fb7b44058c57fb94
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-11-03 03:40:20 +0500

    update

--- functions/process-check-moderation.js
+++ functions/process-check-moderation.js
@@ -10,16 +10,20 @@ const { DB, DATASHEETNAME } = constants;
 const task = cron.schedule('* * * * *', async () => {
     try {
         const data_obj = await check_success_moderation();
-        const success_obj = {};
 
         if (Object.keys(data_obj).length > 0) {
             Object.values(data_obj).forEach(({ chat_id, type, uid, i, col_letter }, index) => {
                 try {
                     const success = send_first_messages(chat_id, type, uid);
                     if (success) {
-                        if (!success_obj[index]) success_obj[index] = {};
-                        success_obj[index] = { col_letter, i };
                         logger.info('Initial messages sent successfully');
+                        const range = `${DATASHEETNAME}!${col_letter}${i}`;
+                        const requestBody = { values: [[true]] };
+                        const { data } = update_data(DB, range, requestBody);
+                        logger.info(data);
+                        if (data.spreadsheetId) {
+                            logger.info(`Check_server set to TRUE`);
+                        }
                     }
                 } catch (error) {
                     logger.error(`Error sending initial messages: ${error}`);
@@ -28,18 +32,6 @@ const task = cron.schedule('* * * * *', async () => {
             });
         } else {
             logger.info('There are no users to send initial messages');
-        }
-
-        if (Object.keys(success_obj).length > 0) {
-            Object.values(success_obj).forEach(({ col_letter, i }) => {
-                const range = `${DATASHEETNAME}!${col_letter}${i}`;
-                const requestBody = { values: [[true]] };
-                const { data } = update_data(DB, range, requestBody);
-                logger.info(data);
-                if (data.spreadsheetId) {
-                    logger.info(`Check_server set to TRUE`);
-                }
-            });
         }
 
     } catch (error) {
