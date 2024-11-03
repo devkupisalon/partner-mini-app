dc1df0deb4a02196cca094c8187228af1d7af4bd
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-11-03 19:27:56 +0500

    update

--- code/personal-data-checkbox.js
+++ code/personal-data-checkbox.js
@@ -2,12 +2,12 @@ const checkbox = document.getElementById('personal-data-checkbox');
 const label_personal_data = document.getElementById('personal-data-id');
 
 // Слушаем изменения состояния чекбокса
-checkbox.addEventListener('change', function() {
+checkbox.addEventListener('change', function () {
     if (checkbox.checked) {
-        tg,MainButton.disabled(); 
-        label_personal_data.style.color = 'initial'; 
+        tg.MainButton.enabled();
+        label_personal_data.style.color = 'initial';
     } else {
-        tg.MainButton.enabled(); 
+        tg.MainButton.disabled();
         label_personal_data.style.color = 'red';
     }
 });
