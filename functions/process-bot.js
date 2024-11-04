99d20084ad787e91974578a13d757c847b91faea
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-11-04 09:30:19 +0500

    update

--- functions/process-bot.js
+++ functions/process-bot.js
@@ -141,7 +141,7 @@ bot.on('message', async (message) => {
         logger.info(message);
         logger.info(type_m);
 
-        text = `Агент *${partner_name}:* ${text}\n\nID:\[${partner_id}\]\nmessage_id:{${messageId}}\n`;
+        text = `Агент *${partner_name}:* ${text}\n\nID:${partner_id}\nmessage_id:{${messageId}}\n`;
 
         /** MEDIA FUNCTIONS */
         const l_message = (l) => { return `${l} message successfully sended from chat_id ${id} to group_chat_id ${GROUP_CHAT_ID}` };
