dfeb7c2bda6f91b17e26cab1d5b94e3acc8fa4b0
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-11-05 01:40:55 +0500

    update

--- functions/process-bot.js
+++ functions/process-bot.js
@@ -401,7 +401,7 @@ bot.on('message', async (message) => {
                         return c_chat_id === chat_id && v.hash_id === hash_id && v.data && v.data.length > 0;
                     });
 
-                    media_data = selectedData ? selectedData[1].data : [[{ media, mime_type: !media.mimeType ? 'image/png' : media.mimeType }]];
+                    media_data = selectedData ? selectedData[1].data : [[{ media: media.file_id, mime_type: !media.mimeType ? 'image/png' : media.mimeType }]];
 
                     const { partner_folder } = await get_partner_name_and_manager(agent_id);
                     const folder = await create_folder(`${hash_id}-${agent_name}`, partner_folder);
