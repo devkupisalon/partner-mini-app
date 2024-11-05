0f50042c0ae0ce5eb5b56247d098a1a8191d557a
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-11-05 22:53:53 +0500

    update

--- code/personal-data-checkbox.js
+++ code/personal-data-checkbox.js
@@ -3,10 +3,9 @@ const label_personal_data = document.getElementById('personal-data-id');
 
 /** CHECKBOX CHANGE */
 checkbox.addEventListener('change', function () {
-    const color = label_personal_data.style.color;
     if (checkbox.checked) {
         tg.MainButton.show();
-        label_personal_data.style.color = color;
+        label_personal_data.style.color = tg.ThemeParams.button_color;
     } else {
         tg.MainButton.hide();
         label_personal_data.style.color = 'red';
