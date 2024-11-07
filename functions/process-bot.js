745ce3d4c652b19aa5893897b302fde5d25d7185
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-11-07 17:37:41 +0500

    update

--- functions/process-bot.js
+++ functions/process-bot.js
@@ -497,7 +497,7 @@ const process_save = async (data) => {
                     agent_id = d.agent_id;
                     agent_name = d.agent_name
                     chat_id = d.chat_id;
-                    return c_chat_id === d.chat_id && hash === d.hash_id && v?.mesasge_ids[0] === d.agent_message_id && v.data && v.data.length > 0;
+                    return c_chat_id === d.chat_id && hash === d.hash_id && v?.messasge_ids[0] === d.agent_message_id && v.data && v.data.length > 0;
                 } else {
                     return c_chat_id === chat_id && v.hash_id === hash_id && v.data && v.data.length > 0;
                 }
