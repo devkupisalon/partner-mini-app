52a14077ee22b5180f9ee964d3a54d41104e3796
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-11-17 22:57:30 +0500

    update

--- functions/helper.js
+++ functions/helper.js
@@ -1,5 +1,5 @@
 import bot from "./bot/init-bot.js";
-import logger from "../../logs/logger.js";
+import logger from "../logs/logger.js";
 import { append_json_file } from "./process-json.js";
 
 /**
