c8f38eb7eb8c172a69df2d7a7c316a45ffed6077
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-11-18 00:50:48 +0500

    update

--- functions/bot/process-media-group.js
+++ functions/bot/process-media-group.js
@@ -59,7 +59,8 @@ const send_media_group = async () => {
                     if (from_user) process_save_media_to_obj(message, user_id, hash_id);
                     delete media_obj[Object.keys(media_obj)[i]];
                     await process_write_json(send_media_obj_path, media_obj);
-                    logger.info(media_obj);
+                    const new_media_obj = await process_return_json(send_media_obj_path);
+                    logger.info(new_media_obj);
                 }
             }
         }
