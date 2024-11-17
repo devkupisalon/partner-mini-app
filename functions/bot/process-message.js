661a7b63e8003e9e27c26db11f19c664394a29ae
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-11-17 23:57:31 +0500

    update

--- functions/bot/process-message.js
+++ functions/bot/process-message.js
@@ -89,6 +89,8 @@ const process_message = async (data) => {
                 chat_id: CHAT_ID,
             };
         if (message.caption) {
+            logger.info(message.caption);
+            logger.info(message);
             send_media_obj[id].caption = from_user
                 ? `Агент [${partner_name}](${partner_url}):\n\n${message.caption}\n\n\`${hash}\``
                 : text;
