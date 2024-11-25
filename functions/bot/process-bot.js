25d23acab5c3e64c4e39a00f559fe1ba1c633b12
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-11-26 03:41:22 +0500

    update

--- functions/bot/process-bot.js
+++ functions/bot/process-bot.js
@@ -115,7 +115,7 @@ bot.on("message", async (message) => {
   // }
 
   const partner_url = `${DBLINK}&range=${row}:${row}`;
-  logger.info({ partner_name, partner_url });
+  logger.info({ partner_name, partner_url, partner_id });
 
   // Process agent messages
   if (partner_name && partner_id && !is_group && !is_bot && !is_manager) {
--- functions/bot/process-google.js
+++ functions/bot/process-google.js
@@ -135,10 +135,6 @@ const process_save = async (data) => {
             hash_id = d.hash_id;
         }
 
-        logger.info(reply_to_message_id);
-        logger.info(is_include_groups);
-        logger.info(hash_id);
-
         const media_obj = await process_return_json(media_files_obj_path);
 
         const selectedData = Object.entries(media_obj).find(([k, v]) => {
