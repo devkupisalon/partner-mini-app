cf9fd8be8f03933848be4ff7e99f36e4b551c809
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-11-25 12:23:48 +0500

    update

--- functions/bot/process-init-messages.js
+++ functions/bot/process-init-messages.js
@@ -171,7 +171,7 @@ const set_chat_photo = async (chatId, root_chat_id) => {
     // console.log(fileStream);
 
     try {
-        const result = await bot.setChatPhoto(chatId, fileStream);
+        const result = await bot.setChatPhoto(chatId, formData);
         logger.info(`Photo set successfully: ${result}`);
     } catch (error) {
         logger.error(`Error setting photo: ${error}`);
