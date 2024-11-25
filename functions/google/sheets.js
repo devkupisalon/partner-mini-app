8fbe518def17a9ec0943af46297daf1aeab22da9
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-11-26 04:07:26 +0500

    update

--- functions/google/sheets.js
+++ functions/google/sheets.js
@@ -261,11 +261,7 @@ const do_calc = async (params) => {
     date,
   ];
 
-  logger.info(partner_folder);
-
   partner_folder = folderId ? folderId : partner_folder;
-
-  logger.info(partner_folder);
 
   try {
     const values = await get_data(MONITORSPREADSHEET, MONITORSHEETNAME);
