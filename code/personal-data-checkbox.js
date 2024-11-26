140c6d362b6a71d2977d6d2b12fe05465a247b99
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-11-26 13:56:22 +0500

    update

--- code/personal-data-checkbox.js
+++ code/personal-data-checkbox.js
@@ -2,7 +2,7 @@ const checkbox = document.getElementById('personal-data-checkbox');
 const label_personal_data = document.getElementById('personal-data-id');
 const privacy_policy_link = document.querySelector('a[href="ссылка_на_обработку"]');
 const condition_link = document.querySelector('a[href="ссылка_на_условия"]');
-const privacy_policy = "ссылка_на_обработку";
+const privacy_policy = "https://docs.google.com/document/d/1-97d6mWROAveReH3bovUirABGMwWdWD2xjpG3mjRa-A/edit?usp=sharing";
 const condition = "https://docs.google.com/document/d/15froq8rgY1NWQr4UatepF7qryptLViudE7S58tfAj-Q/edit?usp=sharing";
 
 condition_link ? condition_link.addEventListener('click', function (event) {
