57b1eefb413d16b6cf27f74384182840a3351d24
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-11-26 10:43:45 +0500

    update

--- code/mask.js
+++ code/mask.js
@@ -1,5 +1,4 @@
 /** inputmask for percentage */
-const percent_input = document.getElementById('partner-percent');
 $(document).ready(function() {
     $('#partner-percent').inputmask({"mask": "99,99%"});
   });
