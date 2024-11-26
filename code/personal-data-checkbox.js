07520b792ce9c1bf7d98d32b930c08dc69a5ec0c
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-11-26 11:15:23 +0500

    update

--- code/personal-data-checkbox.js
+++ code/personal-data-checkbox.js
@@ -1,19 +1,19 @@
 const checkbox = document.getElementById('personal-data-checkbox');
 const label_personal_data = document.getElementById('personal-data-id');
-const privacy_policy_link = document.getElementById("privacy-policy");
-const condition_link = document.getElementById("conditions");
-const privacy_policy= "ссылка_на_обработку";
+const privacy_policy_link = document.querySelector('a[href="ссылка_на_согласие"]');
+const condition_link = document.querySelector('a[href="ссылка_на_условия"]');
+const privacy_policy = "ссылка_на_обработку";
 const condition = "https://docs.google.com/document/d/15froq8rgY1NWQr4UatepF7qryptLViudE7S58tfAj-Q/edit?usp=sharing";
 
-condition_link.addEventListener('click', function(event) {
-  event.preventDefault();
-  tg.openLink(condition);
+condition_link.addEventListener('click', function (event) {
+    event.preventDefault();
+    tg.openLink(condition);
 });
 
-privacy_policy_link.addEventListener('click', function(event) {
+privacy_policy_link.addEventListener('click', function (event) {
     event.preventDefault();
     tg.openLink(privacy_policy);
-  });
+});
 
 
 /** CHECKBOX CHANGE */
